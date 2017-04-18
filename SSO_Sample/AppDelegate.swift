77cfe924003447cf779070cfa9b5d98ccd6ab814
Sivajee Battina <sivajee@zoojoo.be>
2017-04-18 16:23:32 +0530

    Sample done

--- SSO_Sample/AppDelegate.swift
+++ SSO_Sample/AppDelegate.swift
@@ -40,6 +40,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+        
+        print("app: \(app)")
+        // print OAuth response URL
+        print("url: \(url)")
+        print("options: \(options)")
+        
+        
+        NSNotificationCenter.defaultCenter().postNotificationName(kSafariViewControllerCloseNotification, object: url)
+        
+        return true
+    }
 
 
 }
--- SSO_Sample/ViewController.swift
+++ SSO_Sample/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+let kSafariViewControllerCloseNotification = "kSafariViewControllerCloseNotification"
 
+// facebook OAuth URL
+let authURL = NSURL(string: "https://www.appzo2jo2bexx11-stage.in/suio?consumer_key=lInXLgx6HbF9FFq1ZQN8iSEnhzO3JVuf&consumer_secret=6kvwVhDGUQGXFawmulAhvORRV8HpZy5OMqMVH7xqwkLcvTbo")
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
+    
+    var safariVC: SFSafariViewController?
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+        safariVC = SFSafariViewController(URL: authURL!)
+        safariVC!.delegate = self
+        self.presentViewController(safariVC!, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.safariLogin(_:)), name: kSafariViewControllerCloseNotification, object: nil)
     }
-
+    
+    func safariLogin(notification: NSNotification) {
+        // get the url from the auth callback
+        let url = notification.object as! NSURL
+        // Finally dismiss the Safari View Controller with:
+        self.safariVC!.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func safariViewControllerDidFinish(controller: SFSafariViewController) {
+        controller.dismissViewControllerAnimated(true) { () -> Void in
+            print("You just dismissed the login view.")
+        }
+    }
+    
+    func safariViewController(controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+        print("didLoadSuccessfully: \(didLoadSuccessfully)")
+        
+    }
 }
-
